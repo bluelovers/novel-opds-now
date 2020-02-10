2dcf2ff9a616b21142e88deff75149ccc1e59a29
bluelovers <codelovers@users.sourceforge.net>
2020-02-10 15:30:56 +0800

    chore(release): publish

--- test/test.ts
+++ test/test.ts
@@ -8,7 +8,7 @@ import fetch from 'cross-fetch';
 import Bluebird from 'bluebird';
 import { EnumIDKEYListString } from 'novel-downloader/src/all/const';
 
-let id = String(2682);
+let id = String(2685);
 let IDKEY: EnumIDKEYListString = 'wenku8';
 
 // @ts-ignore
@@ -19,7 +19,8 @@ console.log(`wait a long time`);
 Bluebird.resolve()
 	.thenReturn(fetch(`http://localhost:3000/file/${IDKEY}/${id}`))
 	.catch(e => null)
-	.finally(async () => {
+	.finally(async () =>
+	{
 
 		console.log(`ok again, wait 3s`);
 		await Bluebird.delay(3000);
@@ -27,11 +28,11 @@ Bluebird.resolve()
 		// @ts-ignore
 		let gun = new Gun({
 			peers: [
-				//"http://localhost:3000/gun",
+				"http://localhost:3000/gun",
 				//"https://gunjs.herokuapp.com/gun",
-				"http://nmr.io:8765/gun",
+				//"http://nmr.io:8765/gun",
 				"https://my-test-gun-server.herokuapp.com/gun",
-			]
+			],
 		});
 
 		console.log(`ok again, wait 3s`);
@@ -39,15 +40,20 @@ Bluebird.resolve()
 
 		console.log(222)
 
-		await gun
+		let gunNode = gun
 			.get('epub-file')
 			.get(IDKEY)
-			//.get(id)
+			.get(id)
+			;
+
+		await retryNode(gunNode)
 			//.get('filename')
-			.then(raw => {
+			.then(raw =>
+			{
 
 				try
 				{
+					// @ts-ignore
 					delete raw.base64;
 				}
 				catch (e)
@@ -72,11 +78,39 @@ Bluebird.resolve()
 //			.then(v => console.log(`here should return { timestamp, base64 }, but`, v))
 //		;
 
-
-
-
 	})
-	//.catch(e => null)
+	.tapCatch(e => console.error(e))
+	.finally(() => process.exit())
 ;
 
+function retryNode(gunNode, maxRetryAttempts = 10)
+{
+	maxRetryAttempts = Math.max(maxRetryAttempts | 0, 0);
+	let retryAttempts = 0;
+	return new Bluebird((resolve, reject) => {
+		function f()
+		{
+			gunNode
+				.once(function (a)
+				{
+					for (let k in a)
+					{
+						if (k === '_' || k === '#') continue;
+						return resolve(a);
+					}
 
+					if (retryAttempts >= maxRetryAttempts)
+					{
+						console.log(`give up retry, ${retryAttempts}`);
+						return reject(a)
+					}
+
+					console.log(`retry again, ${++retryAttempts}`);
+					setTimeout(f, 1000);
+				})
+			;
+		}
+
+		f();
+	})
+}
