056b3b25e4cd3b6af1c00e41ffa8ddad32c0c8e2
bluelovers <codelovers@users.sourceforge.net>
2020-03-07 01:57:29 +0800

    .

--- lib/opds/index.d.ts
+++ lib/opds/index.d.ts
@@ -2,6 +2,6 @@
 import { ISiteIDs } from '../novel-cache/types';
 import { OPDSV1 } from 'opds-extra';
 export declare function makeOPDSShared(feed: OPDSV1.Feed, msg?: string): OPDSV1.Feed;
-export declare function makeOPDSSite(siteID: ISiteIDs): import("bluebird")<OPDSV1.Feed>;
+export declare function makeOPDSSite(siteID: ISiteIDs, searchTerm?: string): import("bluebird")<OPDSV1.Feed>;
 export declare function makeOPDSPortal(): OPDSV1.Feed;
 export default makeOPDSPortal;
