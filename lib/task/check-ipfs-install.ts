16fb535f42f270911300f2012f1578e824dae79b
bluelovers <codelovers@users.sourceforge.net>
2021-07-10 16:12:36 +0800

    fix(install): fix postinstall

--- lib/task/check-ipfs-install.ts
+++ lib/task/check-ipfs-install.ts
@@ -2,6 +2,7 @@
  * Created by user on 2021/7/10.
  */
 import Bluebird from 'bluebird';
+import { dirname } from 'path';
 //import console from 'debug-color2/logger';
 
 export function checkIpfsInstall()
@@ -9,13 +10,21 @@ export function checkIpfsInstall()
 	return Bluebird.resolve<{
 			path(): string,
 		}>(import('go-ipfs'))
-		.then(m => m.path())
-		//.tap(m => Promise.reject('debug test'))
-		.catch(async (e) => {
-			console.warn(`checkIpfsInstall`, String(e))
+		.then(m => {
+			return Bluebird.resolve(m).then(m => m.path())
+				//.tap(m => Promise.reject('debug test'))
+				.catch(async (e) => {
+					console.warn(`checkIpfsInstall`, String(e))
 
-			const download: () => string = await import('go-ipfs/src/download').then(m => m.default ?? m);
-			return download()
+					let cwd = dirname(require.resolve('go-ipfs/package.json'));
+
+					const download: (version?: string, platform?: string, arch?: string, installPath?: string) => string = await import('go-ipfs/src/download').then(m => m.default ?? m);
+					return download(null, null, null, cwd)
+				})
+				.catch(async (e) => {
+					// Error: EEXIST: file already exists, symlink
+					return Promise.resolve().then(() => m.path()).catch(() => Promise.reject(e))
+				})
 		})
 	;
 }
