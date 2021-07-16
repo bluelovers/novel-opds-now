c013412e46c6d70661f70a3eb7a43a2d3bf24031
bluelovers <codelovers@users.sourceforge.net>
2021-07-17 00:57:06 +0800

    feat: update initMutableFileSystem

--- lib/ipfs/mfs/initMutableFileSystem.ts
+++ lib/ipfs/mfs/initMutableFileSystem.ts
@@ -2,6 +2,7 @@ import { ITSResolvable } from 'ts-type/lib/generic';
 import { IUseIPFSApi } from '../../types';
 import Bluebird from 'bluebird';
 import console from 'debug-color2/logger';
+import { StatResult } from 'ipfs-core-types/src/files';
 
 export function initMutableFileSystem(ipfs: ITSResolvable<IUseIPFSApi>)
 {
@@ -13,11 +14,22 @@ export function initMutableFileSystem(ipfs: ITSResolvable<IUseIPFSApi>)
 				preload: true,
 			});
 
-			await ipfs.files.cp(`/ipfs/${ret.cid}`, `/novel-opds-now/Hello from novel-opds-now Checker.txt`, {
-				// @ts-ignore
-				pin: false,
-				parents: true,
+			let file_cid = ret.cid;
+			let file_path = `/novel-opds-now/Hello from novel-opds-now Checker.txt`;
+
+			let file_stat: StatResult = await ipfs.files.stat(file_path, {
+				hash: true,
 			}).catch(e => null);
+
+			if (file_stat.cid != file_cid)
+			{
+				await ipfs.files.rm(file_path).catch(e => null);
+				await ipfs.files.cp(`/ipfs/${file_cid}`, file_path, {
+					// @ts-ignore
+					pin: false,
+					parents: true,
+				});
+			}
 
 			//console.debug(`[IPFS]`, `initMutableFileSystem`, ret)
 
