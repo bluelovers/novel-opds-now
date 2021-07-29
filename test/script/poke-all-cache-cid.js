8a6bde9bfee60b623771ab839e270d2bc5e87039
bluelovers <codelovers@users.sourceforge.net>
2021-07-29 23:11:57 +0800

    ci: update poke script

--- test/script/poke-all-cache-cid.js
+++ test/script/poke-all-cache-cid.js
@@ -3,16 +3,56 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const deepEntryListMap_1 = require("../../lib/ipfs/mfs/deepEntryListMap");
 const pokeAll_1 = (0, tslib_1.__importStar)(require("../../lib/ipfs/pokeAll"));
-exports.default = (0, deepEntryListMap_1.loadDeepEntryListMapFromMixin)()
-    .then(deepEntryListMap => [...deepEntryListMap])
-    .map(([path, cid], index, length) => {
+const raceFetchServerList_1 = require("../../lib/util/raceFetchServerList");
+const array_hyper_unique_1 = require("array-hyper-unique");
+const bluebird_1 = (0, tslib_1.__importDefault)(require("bluebird"));
+const events_1 = require("events");
+let oldSet = new Set();
+exports.default = bluebird_1.default.resolve()
+    .then(async () => {
+    let ls = [];
+    events_1.EventEmitter.defaultMaxListeners = 50;
+    process.setMaxListeners(50);
+    await (0, deepEntryListMap_1.loadDeepEntryListMapFromFile)().then(m => ls.push(...m));
+    console.debug(`loadDeepEntryListMapFromFile`, ls.length);
+    await _json();
+    await (0, deepEntryListMap_1.loadDeepEntryListMapFromServer)().then(m => ls.push(...m));
+    console.debug(`loadDeepEntryListMapFromServer`, ls.length);
+    await _json();
+    async function _json() {
+        let json_path = deepEntryListMap_1.deepEntryListMap.get((0, deepEntryListMap_1.pathDeepEntryListMapJson)());
+        if ((json_path === null || json_path === void 0 ? void 0 : json_path.length) && !oldSet.has(json_path)) {
+            await (0, raceFetchServerList_1.raceFetchAll)((0, raceFetchServerList_1.raceFetchServerList)(null, json_path), 60 * 1000)
+                .then(buf => JSON.parse(String(buf)))
+                .tap(row => {
+                if (!row.length) {
+                    const e = new TypeError(`deepEntryListMap data is broken`);
+                    e.data = row;
+                    return Promise.reject(e);
+                }
+            })
+                .tap((map) => {
+                let tmp = new Map();
+                (0, deepEntryListMap_1.mergeDeepEntryListMap)(map, tmp);
+                (0, deepEntryListMap_1.fixDeepEntryListMap)(tmp);
+                ls.push(...tmp);
+                console.debug((0, deepEntryListMap_1.pathDeepEntryListMapJson)(), json_path, ls.length);
+            })
+                .catchReturn(null)
+                .finally(() => deepEntryListMap_1.deepEntryListMap.delete((0, deepEntryListMap_1.pathDeepEntryListMapJson)()));
+        }
+        oldSet.add(json_path);
+    }
+    ls = (0, array_hyper_unique_1.array_unique_overwrite)(ls);
+    console.debug(`array_unique_overwrite`, ls.length);
+    return ls;
+})
+    .each(([path, cid], index, length) => {
     let label = `${index.toString().padStart(5, '0')}／${length.toString().padStart(5, '0')}`;
     return (0, pokeAll_1.default)(cid, null, {
         timeout: 10 * 1000,
     }, label, path)
         .tap(ls => (0, pokeAll_1.reportPokeAllSettledResult)(ls, label, path))
         .catchReturn(null);
-}, {
-    concurrency: 5,
 });
 //# sourceMappingURL=poke-all-cache-cid.js.map
--- test/script/poke-all-cache-cid.ts
+++ test/script/poke-all-cache-cid.ts
@@ -1,13 +1,80 @@
 import {
-	deepEntryListMap,
+	deepEntryListMap, fixDeepEntryListMap,
 	loadDeepEntryListMapFromFile,
-	loadDeepEntryListMapFromMixin,
+	loadDeepEntryListMapFromMixin, loadDeepEntryListMapFromServer, mergeDeepEntryListMap, pathDeepEntryListMapJson,
 } from "../../lib/ipfs/mfs/deepEntryListMap";
 import pokeAll, { reportPokeAllSettledResult } from '../../lib/ipfs/pokeAll';
+import { raceFetchAll, raceFetchServerList } from '../../lib/util/raceFetchServerList';
+import { array_unique_overwrite } from 'array-hyper-unique';
+import Bluebird from 'bluebird';
+import { EventEmitter } from 'events';
 
-export default loadDeepEntryListMapFromMixin()
-	.then(deepEntryListMap => [...deepEntryListMap])
-	.map(([path, cid], index, length) =>
+let oldSet = new Set();
+
+export default Bluebird.resolve()
+	.then(async () =>
+	{
+		let ls: [string, string][] = [];
+
+		EventEmitter.defaultMaxListeners = 50;
+		process.setMaxListeners(50);
+
+		await loadDeepEntryListMapFromFile().then(m => ls.push(...m));
+
+		console.debug(`loadDeepEntryListMapFromFile`, ls.length);
+
+		await _json();
+
+		await loadDeepEntryListMapFromServer().then(m => ls.push(...m));
+
+		console.debug(`loadDeepEntryListMapFromServer`, ls.length);
+
+		await _json();
+
+		async function _json()
+		{
+			let json_path = deepEntryListMap.get(pathDeepEntryListMapJson());
+
+			if (json_path?.length && !oldSet.has(json_path))
+			{
+				await raceFetchAll(raceFetchServerList(null, json_path), 60 * 1000)
+					.then(buf => JSON.parse(String(buf) as string) as [string, string][])
+					.tap(row =>
+					{
+						if (!row.length)
+						{
+							const e = new TypeError(`deepEntryListMap data is broken`);
+							// @ts-ignore
+							e.data = row;
+							return Promise.reject(e)
+						}
+					})
+					.tap((map) =>
+					{
+						let tmp = new Map<string, string>();
+						mergeDeepEntryListMap(map, tmp);
+						fixDeepEntryListMap(tmp);
+
+						ls.push(...tmp);
+
+						console.debug(pathDeepEntryListMapJson(), json_path, ls.length);
+
+					})
+					.catchReturn(null as null)
+					.finally(() => deepEntryListMap.delete(pathDeepEntryListMapJson()))
+				;
+			}
+
+			oldSet.add(json_path);
+		}
+
+		ls = array_unique_overwrite(ls);
+
+		console.debug(`array_unique_overwrite`, ls.length);
+
+		return ls
+	})
+	.each(([path, cid], index, length) =>
 	{
 		let label = `${index.toString().padStart(5, '0')}／${length.toString().padStart(5, '0')}`
 
@@ -16,7 +83,5 @@ export default loadDeepEntryListMapFromMixin()
 		}, label, path)
 			.tap(ls => reportPokeAllSettledResult(ls, label, path))
 			.catchReturn(null as null)
-	}, {
-		concurrency: 5,
 	})
 ;
